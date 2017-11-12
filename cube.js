771547d9680462e7e19873993b66657585668c47
Pedro Sousa <pedroteosousa@Pedros-MacBook-Pro.local>
2017-11-12 16:36:33 -0200

    added other moves and created a function to scramble the cube

--- cube.js
+++ cube.js
@@ -1,16 +1,31 @@
 class Cube {
+	// Creates a solved cube at WCA scrambling orientation
 	constructor() {
+		// edge permutation
 		this.ep = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+		// edge orientation
 		this.eo = (new Array (12)).fill(0)
+		// corner permutation
 		this.cp = [0, 1, 2, 3, 4, 5, 6, 7]
+		// corner orientation
 		this.co = (new Array (8)).fill(0)
+		// centers
 		this.c = [0, 1, 2, 3, 4, 5]
 	}
+	// List of moves given by cicles and/or sequences of other moves
 	static moves() {
 		return {
 			"U" : {
 				"corners" : [[0, 1, 2, 3], [0, 0, 0, 0]],
 				"edges" : [[0, 1, 2, 3], [0, 0, 0, 0]]
+			},
+			"D" : {
+				"corners" : [[4, 5, 6, 7], [0, 0, 0, 0]],
+				"edges" : [[8, 9, 10, 11], [0, 0, 0, 0]]
+			},
+			"L" : {
+				"corners" : [[0, 3, 4, 7], [1, 2, 1, 2]],
+				"edges" : [[3, 5, 11, 4], [0, 0, 0, 0]]
 			},
 			"R" : {
 				"corners" : [[2, 1, 6, 5], [1, 2, 1, 2]],
@@ -19,13 +34,40 @@ class Cube {
 			"F" : {
 				"corners" : [[3, 2, 5, 4], [1, 2, 1, 2]],
 				"edges" : [[2, 6, 8, 5], [1, 1, 1, 1]]
-			}
+			},
+			"B" : {
+				"corners" : [[1, 0, 11, 10], [1, 2, 1, 2]],
+				"edges" : [[2, 6, 8, 5], [1, 1, 1, 1]]
+			},
+			"S" : {
+				"edges" : [[1, 3, 11, 9], [1, 1, 1, 1]],
+				"centers" : [0, 1, 5, 3]
+			},
+			"E" : {
+				"edges" : [[7, 6, 5, 4], [1, 1, 1, 1]],
+				"centers" : [4, 3, 2, 1]
+			},
+			"M" : {
+				"edges" : [[0, 2, 8, 10], [0, 0, 0, 0]],
+				"centers" : [0, 2, 5, 4]
+			},
+			"L'" : {"sequence" : "L L L"},
+			"R'" : {"sequence" : "R R R"},
+			"U'" : {"sequence" : "U U U"},
+			"D'" : {"sequence" : "D D D"},
+			"F'" : {"sequence" : "F F F"},
+			"B'" : {"sequence" : "B B B"},
 		}
 	}
+	// Make a move from the list of moves	
 	move(m) {
 		var moveInfo = Cube.moves()[m]
 		var t, pieceInfo
-
+		
+		// === Sequence ===
+		if (moveInfo.sequence != undefined) {
+			this.scramble(moveInfo.sequence)
+		}
 		// === Corners ===
 		if (moveInfo.corners != undefined) {
 			t = moveInfo.corners[0][0]
@@ -60,8 +102,14 @@ class Cube {
 			this.c[moveInfo.centers[1]] = pieceInfo
 		}
 	}
-
+	// Make all moves in a scramble string
+	scramble(s) {
+		var moves = s.split(' ')
+		for (var i in moves) {
+			this.move(moves[i])
+		}
+	}
 }
-
 var cube = new Cube ()
+cube.scramble("R U' R' U' R U R' F' R U R' U' R' F R")
 console.log(cube)
